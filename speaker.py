9224ffa1b15892cb2135fbbccd4de6b4518d8b0e
Akhil Nadigatla <akhilnadigatla@hotmail.com>
2019-02-08 21:50:25 -0500

    Update speaker.py
    NLP Category analysis

--- speaker.py
+++ speaker.py
@@ -135,18 +135,24 @@ def transcribe_file(speech_file):
         print("yes")
 transcribe_file("output.mp3")
 
+# Classification of objects in text
+# def classify_text(text):
+#     """Classifies content categories of the provided text."""
+#     client = language.LanguageServiceClient()
 
+#     if isinstance(text, six.binary_type):
+#         text = text.decode('utf-8')
 
+#     document = types.Document(
+#         content=text.encode('utf-8'),
+#         type=enums.Document.Type.PLAIN_TEXT)
 
+#     categories = client.classify_text(document).categories
 
-
-
-
-
-
-
-
-
+#     for category in categories:
+#         print(u'=' * 20)
+#         print(u'{:<16}: {}'.format('name', category.name))
+#         print(u'{:<16}: {}'.format('confidence', category.confidence))
 #
 # def record_file(time):
 #     language_code = 'en-US'  # a BCP-47 language tag
